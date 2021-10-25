a887c550619de8de64342bf4568a9e13477e1590
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-10-25 19:43:40 +0100

    use nix to provide openjdk11 and remove java home from bash

--- nix/.nixpkgs/darwin-configuration.nix
+++ nix/.nixpkgs/darwin-configuration.nix
@@ -51,4 +51,6 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  environment.variables.JAVA_HOME = "${pkgs.jdk11}/lib/openjdk";
 }
