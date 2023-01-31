083e392c091e0c9f63434ab2557ecc18b25a5c8d
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-01-31 16:13:07 +0000

    android nix shell

---
+++ nix-shells/android.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+let
+  android-nixpkgs = callPackage (import (builtins.fetchGit {
+    url = "https://github.com/tadfisher/android-nixpkgs.git";
+  })) {
+    # Default; can also choose "beta", "preview", or "canary".
+    channel = "stable";
+  };
+
+  android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+    cmdline-tools-latest
+    build-tools-30-0-3
+    platform-tools
+    platforms-android-33
+    emulator
+  ]);
+
+in
+mkShell {
+  buildInputs = [
+    gradle
+    android-sdk
+  ];
+}
