ad487e959eaf7c54d3038ee152bedfc20bc6fd59
Alessandro Candolini <alessandrocandolini@gmail.com>
2025-03-07 21:23:02 +0000

    Remove deprecated nix-daemon.enable=true setting from darwin config

--- nix/.nixpkgs/darwin-configuration.nix
+++ nix/.nixpkgs/darwin-configuration.nix
@@ -57,8 +57,6 @@ in
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
