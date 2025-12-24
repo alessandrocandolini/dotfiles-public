c5fdad06796c4d75dd3b12f57f66cc7d8354b2c5
Alessandro Candolini <alessandrocandolini@gmail.com>
2025-12-24 17:21:48 +0000

    Fix keymap for lua snippet navigation previously overlapping with LSP signature help

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -112,18 +112,18 @@ function M.setup()
 
   local ls = require("luasnip")
 
-  vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  -- Snippet navigation
+  vim.keymap.set({ "i", "s" }, "<C-l>", function()
     if ls.expand_or_jumpable() then
       ls.expand_or_jump()
     end
   end, { silent = true })
 
-  vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  vim.keymap.set({ "i", "s" }, "<C-h>", function()
     if ls.jumpable(-1) then
       ls.jump(-1)
     end
   end, { silent = true })
-
 
   --------------------------------------------------------
   -- LspAttach autocommand (usa on_attach_impl)
