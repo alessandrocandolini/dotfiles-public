bfdd68a310d7406fc27c0c2df99a7bfbf4ed43ca
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-01-21 21:44:39 +0000

    Lazy loading of haskell, scala, and agda plugins

--- nvim/.config/nvim/lua/config/load-plugins.lua
+++ nvim/.config/nvim/lua/config/load-plugins.lua
@@ -5,44 +5,42 @@ local function gh(repo)
   return "https://github.com/" .. repo
 end
 
-local function pack_hooks(ev)
-  local spec = ev.data.spec
-  local name, kind, path = spec.name, ev.data.kind, ev.data.path
-
-  -- fzf: run install script after install/update
-  if name == "fzf" and (kind == "install" or kind == "update") then
-    vim.system({ "sh", "-c", "./install --all" }, { cwd = path })
+local function postProcessingAfterInstallation(ev)
+  if not ev.data or not ev.data.kind or not ev.data.spec or not ev.data.spec.name then
+    return
   end
+  local kind = ev.data.kind
+  local name = ev.data.spec.name
+  local path = ev.data.path
+  if (kind == "install" or kind == "update") then
+    if name == "fzf" then
+      vim.system({ "sh", "-c", "./install --all" }, { cwd = path })
+    end
 
-  -- cornelis: build via stack after install/update
-  if name == "cornelis" and (kind == "install" or kind == "update") then
-    vim.system({ "stack", "build" }, { cwd = path })
+    if name == "cornelis" then
+      vim.system({ "stack", "build" }, { cwd = path })
+    end
   end
 end
 
 function M.setup()
   -- Run post-install/update hooks
-  vim.api.nvim_create_autocmd("PackChanged", { callback = pack_hooks })
+  vim.api.nvim_create_autocmd("PackChanged", { callback = postProcessingAfterInstallation })
 
   -- Always-on plugins
   vim.pack.add({
     gh("rktjmp/lush.nvim"), -- required by jellybeans-nvim
     gh("metalelf0/jellybeans-nvim"),
 
+    { src = gh("junegunn/fzf"), name = "fzf" },
+    gh("junegunn/fzf.vim"),
+    gh("nvim-lua/plenary.nvim"),
     gh("axelf4/vim-strip-trailing-whitespace"),
     gh("windwp/nvim-autopairs"),
     gh("tpope/vim-projectionist"),
 
-    gh("nvim-lua/plenary.nvim"),
-
-    -- fzf (needs post-install hook)
-    { src = gh("junegunn/fzf"), name = "fzf" },
-    gh("junegunn/fzf.vim"),
-
     -- LSPs
     gh("j-hui/fidget.nvim"),
-    gh("scalameta/nvim-metals"),
-    gh("Mrcjkb/haskell-tools.nvim"),
 
     -- completion/snippets
     gh("hrsh7th/nvim-cmp"),
@@ -50,10 +48,43 @@ function M.setup()
     gh("L3MON4D3/LuaSnip"),
     gh("saadparwaiz1/cmp_luasnip"),
 
-    -- Agda
-    gh("kana/vim-textobj-user"),
-    gh("neovimhaskell/nvim-hs.vim"),
-    { src = gh("agda/cornelis"), name = "cornelis" }, -- stack build hook above
+  }, { load = true })
+
+  -- Filetype plugins: ensure installed, but don't source plugin/* yet (startup-safe)
+  vim.pack.add({
+    { src = gh("scalameta/nvim-metals"), name = "nvim-metals" },
+    { src = gh("Mrcjkb/haskell-tools.nvim"), name = "haskell-tools.nvim" },
+
+    -- Agda (and deps)
+    { src = gh("kana/vim-textobj-user"), name = "vim-textobj-user" },
+    { src = gh("neovimhaskell/nvim-hs.vim"), name = "nvim-hs.vim" },
+    { src = gh("agda/cornelis"), name = "cornelis" },
+  }, { load = false })
+
+  -- metals
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "scala",
+    callback = function()
+      vim.cmd("packadd nvim-metals")
+    end,
+  })
+
+  -- haskell
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "haskell", "lhaskell", "cabal" , "stack"},
+    callback = function()
+      vim.cmd("packadd haskell-tools.nvim")
+    end,
+  })
+
+  -- Agda
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "agda",
+    callback = function()
+      vim.cmd("packadd vim-textobj-user")
+      vim.cmd("packadd nvim-hs.vim")
+      vim.cmd("packadd cornelis")
+    end,
   })
 
 end
