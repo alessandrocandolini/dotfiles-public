d15587b07e7a7a9a5e02398503bfbbf5101fd49f
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-01-31 23:57:25 +0000

    better diagnostics desc

--- nvim/.config/nvim/lua/config/diagnostics.lua
+++ nvim/.config/nvim/lua/config/diagnostics.lua
@@ -3,11 +3,11 @@ local M = {}
 function M.setup()
   vim.keymap.set("n", "[c", function()
     vim.diagnostic.goto_prev({ float = true })
-  end, { silent = true })
+  end, { silent = true,desc = "Diagnostics: previous (float)" })
 
   vim.keymap.set("n", "]c", function()
     vim.diagnostic.goto_next({ float = true })
-  end, { silent = true })
+  end, { silent = true , desc = "Diagnostics: next (float)"})
 
   vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { silent = true, desc = "Diagnostics: buffer (loclist)" })
 
@@ -31,6 +31,6 @@ function M.setup()
     if winid and vim.api.nvim_win_is_valid(winid) then
       vim.api.nvim_set_current_win(winid)
     end
-  end, { silent = true, desc = "Diagnostics float (enter)" })
+  end, { silent = true, desc = "Diagnostics float (focused)" })
 end
 return M
