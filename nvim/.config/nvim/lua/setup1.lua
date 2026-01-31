51668b2ec7013c252947907abe8531c5f2354e41
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-01-31 22:53:05 +0000

    extract fzf config to a separate file, with recovery if fzf is not installed

--- nvim/.config/nvim/lua/setup1.lua
+++ nvim/.config/nvim/lua/setup1.lua
@@ -6,69 +6,6 @@ require('config.load-plugins').setup()
 -- appearance of popup menu for autocomplete
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
--- fzf-lua
-local fzf = require("fzf-lua")
-
-fzf.setup({
-  winopts = {
-    split = "belowright 20new",
-    preview = { hidden = true }
-  },
-  files = {
-     file_icons = false,
-     git_icons = false,
-  },
-  fzf_opts = {
-    ["--info"] = "inline",
-    ["--layout"]    = "default",
-  },
-  keymap = {
-    fzf = {
-      ["ctrl-p"] = "toggle-preview",
-      ["ctrl-q"] = "select-all+accept",
-    },
-  },
-  grep = {
-    rg_opts = table.concat({
-      "--column",
-      "--line-number",
-      "--no-heading",
-      "--color=always",
-      "--smart-case",
-      "--hidden",
-      "--glob=!.git/*",
-    }, " "),
-  },
-})
-
-vim.keymap.set("n", "<Leader>f", function()
-    local cmd = 'fd --color=never --hidden --type f --type l --exclude .git'
-    local base = vim.fn.fnamemodify(vim.fn.expand('%'), ':h:.:S')
-    if base ~= '.' then
-     cmd = cmd .. (" | proximity-sort %s"):format(vim.fn.shellescape(vim.fn.expand('%')))
-    end
-    fzf.files({
-      cmd = cmd,
-      fzf_opts = {
-        ['--scheme']    = 'path',
-        ['--tiebreak']  = 'index',
-      }
-    })
-end, { silent = true })
-
-vim.keymap.set("n", "<Leader>r", function()
-    fzf.live_grep()
-end, { silent = true })
-
-vim.keymap.set("n", "grw", function()
-  local w = vim.fn.expand("<cword>")
-  if w == nil or w == "" then
-    return
-  else
-    fzf.grep_cword()
-  end
-end, { silent = true })
-
 -- Diagnostic
 vim.keymap.set("n", "[c", function()
   vim.diagnostic.goto_prev({ float = true })
@@ -113,6 +50,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Other plugins that we wanna load for every projects
+require("config.fzf").setup()
 require("config.projectionist")
 require("nvim-autopairs").setup {}
 require("oil").setup({
