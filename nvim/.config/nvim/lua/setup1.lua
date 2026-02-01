758d546fdc196015f047d27b7c7c67c95c46bea0
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-02-01 01:25:17 +0000

    Move colourscheme and yank highlighting to an appearance.lua and renamed load-plugins to vimpack

--- nvim/.config/nvim/lua/setup1.lua
+++ nvim/.config/nvim/lua/setup1.lua
@@ -1,25 +1,12 @@
 vim.loader.enable()
 
--- install plugins
-require('config.load-plugins').setup()
-
--- appearance of popup menu for autocomplete
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
--- Highlight on yank for visual feedback
-local group = vim.api.nvim_create_augroup("UserConfig", {clear = true})
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = group,
-  callback = function()
-    vim.highlight.on_yank({timeout = 150})
-  end,
-  desc = "highlight yanked text"
-})
-
--- Other plugins that we wanna load for every projects
-require("config.fzf").setup()
+require('config.vimpack').setup()
+require("config.appearance").setup() -- must be after installing the plugins
 require("config.diagnostics").setup()
+require("config.fzf").setup()
 require("config.projectionist").setup()
+
+-- other plugins
 require("nvim-autopairs").setup()
 require("oil").setup({
   view_options = {
@@ -27,7 +14,3 @@ require("oil").setup({
   }
 })
 require('mini.splitjoin').setup()
-
--- Colorscheme
-vim.opt.termguicolors = true
-pcall(vim.cmd.colorscheme, "jellybeans-nvim")
