05e66fa0f19c474794120bd40e17fbefb2d308ed
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-04-08 01:55:49 +0100

    Update nix

--- nix-darwin/darwin-configuration.nix
+++ nix-darwin/darwin-configuration.nix
@@ -10,6 +10,10 @@
         "nix-command"
         "flakes"
       ];
+      trusted-substituters = [ "https://cache.numtide.com" ];
+      trusted-public-keys = [
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      ];
     };
 
     optimise.automatic = true;
