09a7865f8e9e60a9cea9a2dd7b8d5b39b298b715
Alessandro Candolini <alessandrocandolini@gmail.com>
2026-09-17 02:44:30 +0100

    More LSP fixes

--- nvim/.config/nvim/lsp/bashls.lua
+++ nvim/.config/nvim/lsp/bashls.lua
@@ -1,11 +1,6 @@
 local lsp = require('config.lsp')
 return {
   cmd = { "bash-language-server", "start" },
-  settings = {
-    bashIde = {
-      globPattern = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command)",
-    },
-  },
   filetypes = { "bash", "sh" },
   root_markers = { ".git" },
   capabilities = lsp.capabilities,
--- nvim/.config/nvim/lsp/nil_ls.lua
+++ nvim/.config/nvim/lsp/nil_ls.lua
@@ -2,9 +2,11 @@ local lsp = require('config.lsp')
 return {
   cmd = { "nil"},
   settings = {
-    formatting = {
-      command = { "nixfmt" }
-    }
+    ["nil"] = {
+      formatting = {
+        command = { "nixfmt" }
+      }
+    },
   },
   filetypes = { "nix" },
   root_markers = { "flake.nix", "shell.nix", ".git" },
